173e9f0744e87ac3832b73ad69d2597f13c0a2d1
salehsammm <salehsam380@gmail.com>
2024-11-12 22:24:54 +0330

    change dir

---
+++ SocialMedia.Data/Migrations/20241106215140_2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMedia.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "postId",
+                table: "Posts",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Posts",
+                newName: "postId");
+        }
+    }
+}
---
+++ SocialMedia.Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialMedia.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+
+        // Navigation Properties
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
+    }
+}
